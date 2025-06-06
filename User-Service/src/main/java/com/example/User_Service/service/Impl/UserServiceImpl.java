2a90cb3203e33f0a462f65a035a1ebcc86887c96
Shubham Pandey <shubhapandeyitsoftware2022@gmail.com>
2025-06-06 19:32:39 +0530

    i create another work

--- User-Service/src/main/java/com/example/User_Service/service/Impl/UserServiceImpl.java
+++ User-Service/src/main/java/com/example/User_Service/service/Impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.example.User_Service.service.Impl;
 
+import com.example.User_Service.entity.Rating;
 import com.example.User_Service.entity.User;
 import com.example.User_Service.exception.ResourceNotFoundException;
 import com.example.User_Service.repository.UserRepository;
 import com.example.User_Service.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User savedUser(User user) {
@@ -31,7 +41,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getById(String userId) {
-        return userRepository.findById(userId).orElseThrow(() ->
+        User user = userRepository.findById(userId).orElseThrow(() ->
                 new ResourceNotFoundException("User With given id is not found : " + userId));
+        //fetching rating of the abpve user from RATING-SERVICE
+        //http://localhost:8083/ratings/users/1eb75e00-4512-45b0-a284-a2eb178f016c
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/1eb75e00-4512-45b0-a284-a2eb178f016c", ArrayList.class);
+        logger.info("{}",ratingsOfUser);
+        user.setRating(ratingsOfUser);
+        return user;
     }
 }
