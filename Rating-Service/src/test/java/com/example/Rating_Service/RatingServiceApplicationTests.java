129fdaefcab4fe4b872e5c8237b8107960554271
Shubham Pandey <shubhapandeyitsoftware2022@gmail.com>
2025-06-10 18:43:17 +0530

    communication between all the services by RestTemplate and FeignClient

--- Rating-Service/src/test/java/com/example/Rating_Service/RatingServiceApplicationTests.java
+++ Rating-Service/src/test/java/com/example/Rating_Service/RatingServiceApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.Rating_Service;
 
+import com.example.Rating_Service.entiy.Rating;
+import com.example.Rating_Service.service.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RatingServiceApplicationTests {
 
+	@Autowired
+	private RatingService ratingService;
+
 	@Test
 	void contextLoads() {
 	}
+
+	/*@Test
+	void creatingRating(){
+		Rating rating= Rating.builder().rating(10).userId("").hotelId("").feedback("this hotel is very good").build();
+		Rating rating1=ratingService.create(rating);
+		System.out.println("new rating is create");
+
+	}*/
 
 }
--- User-Service/src/main/java/com/example/User_Service/external/services/RatingService.java
+++ User-Service/src/main/java/com/example/User_Service/external/services/RatingService.java
@@ -1,15 +1,27 @@
-/*
 package com.example.User_Service.external.services;
 
 import com.example.User_Service.entity.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
-import java.util.List;
 
 @FeignClient(name = "RATING-SERVICE")
 public interface RatingService {
-    @GetMapping("/ratings/{userId}")
-    List<Rating> getRatingsByUserId(@PathVariable("userId") String userId);
-}*/
+
+    //Post
+    @PostMapping("/ratings")
+    public ResponseEntity<Rating> createRating(Rating values);
+
+    // Put
+    @PutMapping("/ratings/{ratingId}")
+    public ResponseEntity<Rating> updateRating(@PathVariable String ratingId, Rating rating);
+
+    //Delete
+    @DeleteMapping("/ratings/{ratingId}")
+    public void DeleteRating(@PathVariable String ratingId, String rating);
+
+}
